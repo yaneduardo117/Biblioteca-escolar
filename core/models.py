cd94a4d33a5803d62422844a4455b048976569f3
Yan eduardo <yanedu15@gmail.com>
2025-11-28 22:05:18 -0300

    estrutura inicial do banco de dados (Livro, Autor, Categoria)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+    def __str__(self): return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    def __str__(self): return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=13, unique=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    autor = models.ForeignKey(Autor, on_delete=models.PROTECT)
+    ano_publicacao = models.IntegerField()
+    quantidade = models.IntegerField(default=1)
+
+    def __str__(self): return self.titulo
