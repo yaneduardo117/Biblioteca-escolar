11ccfce65a9319e0b2c08aa2a47617b9e401b452
Yan eduardo <yanedu15@gmail.com>
2025-12-08 20:47:39 -0300

    modelo de usuário personalizado

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'core',
 ]
 
+AUTH_USER_MODEL = 'core.Usuario'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
